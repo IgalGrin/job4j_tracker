16456d5f08bfd9f2c4f8d14b33f42cbe298f642c
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-10-22 19:00:17 +0300

    3. Реализация профессий в коде [#6837 #218840]

--- src/main/java/ru/job4j/inheritance/Dentist.java
+++ src/main/java/ru/job4j/inheritance/Dentist.java
@@ -4,8 +4,8 @@ public class Dentist extends Doctor {
     private String specialty;
     private String employed;
 
-    public Dentist(String name, String surname, String education, String birthday, double experience,
-            String specialty, String employed) {
+    public Dentist(String name, String surname, String education, String birthday,
+                   double experience, String specialty, String employed) {
         super(name, surname, education, birthday, experience);
         this.specialty = specialty;
         this.employed = employed;
--- src/main/java/ru/job4j/inheritance/Surgeon.java
+++ src/main/java/ru/job4j/inheritance/Surgeon.java
@@ -4,8 +4,8 @@ public class Surgeon extends Doctor {
     private String specialty;
     private String employed;
 
-    public Surgeon(String name, String surname, String education, String birthday, double experience,
-            String specialty, String employed) {
+    public Surgeon(String name, String surname, String education, String birthday,
+                   double experience, String specialty, String employed) {
         super(name, surname, education, birthday, experience);
         this.specialty = specialty;
         this.employed = employed;
