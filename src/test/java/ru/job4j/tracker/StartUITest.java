e34d9cbcd5c38b47d9325d782d878bc39ff7f159
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-11-05 17:39:51 +0300

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,7 +1,12 @@
 package ru.job4j.tracker;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.*;
 
 public class StartUITest {
@@ -25,5 +30,32 @@ public class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix ROUTER");
         assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void editItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName(), is("edited item"));
+    }
+
+    @Test
+    public void deleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("del item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+        };
+       StartUI.deleteItem(new StubInput(answers), tracker);
+       Item deleted = tracker.findById(item.getId());
+       assertThat((deleted), Matchers.is(nullValue()));
     }
 }
