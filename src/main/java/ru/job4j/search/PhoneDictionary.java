77690b9ad0b4adc42333fadb9b4a9aec7b4b21cb
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-11-29 23:12:46 +0300

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,26 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+    private ArrayList<Person> result = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+       for (int i = 0; i < persons.size(); i++
+             ) {
+           if (persons.get(i).getName().contains(key)
+                   || persons.get(i).getSurname().contains(key)
+                   || persons.get(i).getAddress().contains(key)
+                   || persons.get(i).getPhone().contains(key)) {
+               result.add(i, persons.get(i));
+               break;
+            }
+        }
+        return result;
+    }
+}
