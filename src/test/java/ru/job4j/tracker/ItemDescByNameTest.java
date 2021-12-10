f1aad8f61d2ed87e25ec3160baad85f96a81e51d
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-12-10 19:36:49 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ItemDescByNameTest {
+    @Test
+    public void ascendingSort() {
+        List<Item> items = Arrays.asList(new Item("1"), new Item("4"),
+                new Item("3"), new Item("2"), new Item("5"));
+        List<Item> expected = Arrays.asList(new Item("5"), new Item("4"),
+                new Item("3"), new Item("2"), new Item("1"));
+        Collections.sort(items, new ItemDescByName());
+        assert expected.equals(items);
+    }
+}
