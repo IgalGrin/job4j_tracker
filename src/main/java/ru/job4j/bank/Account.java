02b99f1972df369394c416f57117abe3dc5cc23d
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-12-13 21:35:35 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,31 +2,73 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель счета пользователя.
+ * @author IGOR GRINKO
+ *   @version 1.0
+ */
 public class Account {
+    /**
+     * Поле принимающее номер счета.
+     */
     private String requisite;
+    /**
+     * Поле принимающее значение баланса.
+     */
     private double balance;
 
+    /**
+     * Конструктор принимает на вход номер счета и значение баланса,
+     * далее устанавливает в поля с соответствующими именами
+     * эти значения и возвращает объект.
+     * @param requisite Передаваемый номер счета.
+     * @param balance Передаваемое значение баланса.
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * Метод не принимающий параметров на входе.
+     * @return Возвращает значение поля requisite.
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Метод принимает на вход номер счета и
+     * устанавливает значение в поле requisite.
+     * @param requisite Передаваемый номер счета.
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Метод не принимающий параметров на входе.
+     * @return Возвращает значение поля balance.
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Метод принимает на вход значение balance и
+     * устанавливает значение в поле balance.
+     * @param balance Передаваемое значение balance.
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Переопределение метода equals()
+     * @param o Передаваемый объект.
+     * @return Возвращает true если объекты эквивалентны,
+     * иначе false.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +81,10 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Переопределение метода hashCode()
+     * @return Возвращает 32 битное число типа int.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -2,31 +2,73 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель пользователя банка.
+ * @author IGOR GRINKO
+ *   @version 1.0
+ */
 public class User {
+    /**
+     * Поле принимающее номер паспорта.
+     */
     private String passport;
+    /**
+     * Поле принимающее имя пользователя.
+     */
     private String username;
 
+    /**
+     * Конструктор принимает на вход номер паспорта и имя пользователя,
+     * далее устанавливает в поля с соответствующими именами
+     * эти значения и возвращает объект.
+     * @param passport Передаваемый номер паспорта.
+     * @param username Передаваемое имя пользователя.
+     */
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Метод не принимающий параметров на входе.
+     * @return Возвращает значение поля passport.
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Метод принимает на вход номер паспорта и устанавливает значение
+     * в поле с соответствующим именем.
+     * @param passport Передаваемый номер паспорта.
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Метод не принимающий параметров на входе.
+     * @return Возвращает значение поля username.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Метод принимает на вход имя пользователя и устанавливает значение
+     * в поле с соответствующим именем.
+     * @param username Передаваемое имя пользователя.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Переопределение метода equals()
+     * @param o Передаваемый объект.
+     * @return Возвращает true если объекты эквивалентны,
+     * иначе false.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +81,10 @@ public class User {
         return Objects.equals(passport, user.passport);
     }
 
+    /**
+     * Переопределение метода hashCode()
+     * @return Возвращает 32 битное число типа int.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(passport);
