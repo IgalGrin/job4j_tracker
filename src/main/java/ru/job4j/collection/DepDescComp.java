f8e9a4a56a4d8121b26c26471664e03a8bc593fd
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-12-19 10:19:35 +0300

    2. Отсортировать департаменты [#34352 #241543]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -1,18 +1,16 @@
 package ru.job4j.collection;
 
 import java.util.Comparator;
-import java.util.Objects;
 
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String strOne, String strTwo) {
         String[] str1 = strOne.split("/");
         String[] str2 = strTwo.split("/");
-        for (int i = 0; i < Math.min(str1.length, str2.length); i++) {
-            if (!Objects.equals(str1[i], str2[i])) {
-                return str1[i].compareTo(str2[i]);
-            }
+        int rsl = str2[0].compareTo(str1[0]);
+        if (rsl != 0) {
+            return rsl;
         }
-        return Integer.compare(str1.length, str2.length);
+        return strOne.compareTo(strTwo);
     }
 }
