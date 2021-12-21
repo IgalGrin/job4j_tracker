dbd63ced1533d89223a897234e2d95e3a3c65df2
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-12-21 18:59:49 +0300

    1.2. Функциональный интерфейс [#247497]

---
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -0,0 +1,32 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.*;
+
+public class FunctionalInterfaces {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        BiConsumer<Integer, String> biCon = (i, s) -> map.put(i, s);
+        biCon.accept(1, "one");
+        biCon.accept(2, "two");
+        biCon.accept(3, "three");
+        biCon.accept(4, "four");
+        biCon.accept(5, "five");
+        biCon.accept(6, "six");
+        biCon.accept(7, "seven");
+        System.out.println(map);
+        for (Integer i : map.keySet()) {
+            BiPredicate<Integer, String> biPred = (in, st) -> in % 2 == 0 || st.length() == 4;
+            if (biPred.test(i, map.get(i))) {
+                System.out.println("key: " + i + " value: " + map.get(i));
+            }
+        }
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+        Consumer<String> con = (sf) -> System.out.println(sf);
+        Function<String, String> func = s -> s.toUpperCase();
+        con.accept(func.apply(String.valueOf(sup.get())));
+        }
+    }
