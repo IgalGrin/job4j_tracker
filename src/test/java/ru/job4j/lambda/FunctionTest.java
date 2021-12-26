8d00ac87bcf91955b2aa40f716104a2620e1a332
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-12-26 13:06:25 +0300

    5. Подсчет функции в диапазоне. [#24251]

---
+++ src/test/java/ru/job4j/lambda/FunctionTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class FunctionTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        FunctionUsage function = new FunctionUsage();
+        int a = 2;
+        int b = 1;
+        List<Double> result = function.diapason(5, 8, x -> a * x + b);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenQuadricFunctionThenQuadricResults() {
+        FunctionUsage function = new FunctionUsage();
+        int a = 2;
+        int b = 1;
+        List<Double> result = function.diapason(5, 8, x -> a * Math.pow(x, 2) + b);
+        List<Double> expected = Arrays.asList(51D, 73D, 99D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenComplexFunctionThenComplexResults() {
+        FunctionUsage function = new FunctionUsage();
+        int a = 2;
+        int b = 1;
+        List<Double> result = function.diapason(5, 8, x -> a * Math.pow(x, x) + b);
+        List<Double> expected = Arrays.asList(6251D, 93313D, 1647087D);
+        assertThat(result, is(expected));
+    }
+}
