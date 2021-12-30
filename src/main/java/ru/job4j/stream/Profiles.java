8c472bc7205027cdd8f0d025f2f74fea3bef5174
IgalGrin <89865761+IgalGrin@users.noreply.github.com>
2021-12-30 09:46:12 +0300

    2. Список адресов [#110062#245185]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,7 +1,5 @@
 package ru.job4j.stream;
 
-
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,14 +7,6 @@ public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-                .collect(Collectors.toList());
-    }
-
-    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
-        return profiles.stream()
-                .map(Profile::getAddress)
-                .sorted(Comparator.comparing(Address::getCity))
-                .distinct()
                 .collect(Collectors.toList());
     }
 }
